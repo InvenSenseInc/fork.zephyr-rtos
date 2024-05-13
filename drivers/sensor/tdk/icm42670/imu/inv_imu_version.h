7dbf9c87f8a402a9b9c1c14a98891f6e828188ad
Aurelie Fontaine <aurelie.fontaine@tdk.com>
2024-05-13 10:20:32 +0200

    drivers: sensor: icm42670: supports icm42670-P/-S
    samples: sensor: tdk_apex: adds sample to support APEX features
    samples: sensor: 6dof_motion_drdy: adds sample to support 6-axis sensor
    data report
    
    Adds official TDK Invensense Inc. driver for icm42670-P/-S sensor.
    Adds SPI and I2C interface support.
    Adds APEX features, such as Pedometer, Tilt detection, Wake on Motion
    and Significant Motion Detector.
    Add generic samples supporting this driver.
    Validated with custom setup: nrf52dk_nrf52832 + icm42670-P/-S EVB board
    Build ok by running:
    west twister -T samples\sensor\tdk_apex
    -T samples\sensor\6dof_motion_drdy -p nrf52dk/nrf52832
    
    Signed-off-by: Aurelie Fontaine <aurelie.fontaine@tdk.com>

---
+++ drivers/sensor/tdk/icm42670/imu/inv_imu_version.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 TDK Invensense
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _INV_IMU_VERSION_H_
+#define _INV_IMU_VERSION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define INV_IMU_VERSION_STRING "3.0.0-dev2"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INV_IMU_VERSION_H_ */
