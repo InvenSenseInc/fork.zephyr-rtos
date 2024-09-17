93805790f24adbe6987cda3a988d75b9d8a9b5fc
remi.buisson <remi.buisson@tdk.com>
2024-09-17 15:48:12 +0200

    Preparing ICP101xx driver + samples working with HAL TDK

---
+++ drivers/sensor/tdk/icp101xx/icp101xx_drv.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 TDK Invensense
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_ICP101XXX_ICP101XXX_H_
+#define ZEPHYR_DRIVERS_SENSOR_ICP101XXX_ICP101XXX_H_
+
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/sensor.h>
+#include "Icp101xx.h"
+#include "Icp101xxSerif.h"
+
+#define DT_DRV_COMPAT invensense_icp101xx
+
+#define ICP101XX_BUS_I2C DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+
+struct icp101xx_data {
+	int raw_pressure;
+	int raw_temperature;
+	float pressure;
+	float temperature;
+	inv_icp101xx_t icp_device;
+};
+
+struct icp101xx_config {
+	struct i2c_dt_spec i2c;
+	int mode;
+};
+
+#endif
