a01796c91f42343491371159d276476e99c59525
Remi Buisson <remi.buisson@tdk.com>
2026-02-11 19:06:53 +0100

    Support 4 Newport parts: icm45605 icm45605s icm45686 icm45686s

--- drivers/sensor/tdk/icm45686/icm45686.c
+++ drivers/sensor/tdk/icm45686/icm45686.c
@@ -568,4 +568,18 @@ static int icm45686_init(const struct device *dev)
 				     CONFIG_SENSOR_INIT_PRIORITY,				   \
 				     &icm45686_driver_api);
 
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT invensense_icm45605
+DT_INST_FOREACH_STATUS_OKAY(ICM45686_INIT)
+
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT invensense_icm45605s
+DT_INST_FOREACH_STATUS_OKAY(ICM45686_INIT)
+
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT invensense_icm45686
+DT_INST_FOREACH_STATUS_OKAY(ICM45686_INIT)
+
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT invensense_icm45686s
 DT_INST_FOREACH_STATUS_OKAY(ICM45686_INIT)
